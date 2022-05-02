8b253b7371a95c7e558c442a621569aaba1ef6ac
Peter Neil Ilogon <60372694+Peter-Ilogon@users.noreply.github.com>
2022-05-02 20:11:19 +0800

    first commit

---
+++ morse_codes.dart
@@ -0,0 +1,54 @@
+// ignore_for_file: unused_local_variable, avoid_types_as_parameter_names, non_constant_identifier_names
+
+import 'dart:io';
+
+void main() {
+  String? uInput;
+
+  print("Input/s: ");
+  uInput = stdin.readLineSync();
+
+  print(decodeMorse(uInput!));
+}
+
+decodeMorse(m) {
+  const ref = {
+    '.-': 'A',
+    '-...': 'B',
+    '-.-.': 'C',
+    '-..': 'D',
+    '.': 'E',
+    '..-.': 'F',
+    '--.': 'G',
+    '....': 'H',
+    '..': 'I',
+    '.---': 'J',
+    '-.-': 'K',
+    '.-..': 'L',
+    '--': 'M',
+    '-.': 'N',
+    '---': 'O',
+    '.--.': 'P',
+    '--.-': 'Q',
+    '.-.': 'R',
+    '...': 'S',
+    '-': 'T',
+    '..-': 'U',
+    '...-': 'V',
+    '.--': 'W',
+    '-..-': 'X',
+    '-.--': 'Y',
+    '--..': 'Z',
+    '.----': '1',
+    '..---': '2',
+    '...--': '3',
+    '....-': '4',
+    '.....': '5',
+    '-....': '6',
+    '--...': '7',
+    '---..': '8',
+    '----.': '9',
+    '-----': '0',
+  };
+}
+//peterneililogon
